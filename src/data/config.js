6b0a2eaecaeea07899118fedf250c3ed0cebf840
David Ohayon <davideytanohayon@gmail.com>
2021-05-12 21:46:57 +0300

    initial commit

---
+++ src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'John Doe',
+  logo: 'https://www.safepoki.com/favicon/favicon-512.png',
+  author: 'John Doe',
+  url: 'https://www.safepoki.com/',
+  legalName: 'DOM',
+  defaultDescription: '',
+  socialLinks: {
+    twitter: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+    youtube: '',
+    google: '',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2021',
+};
